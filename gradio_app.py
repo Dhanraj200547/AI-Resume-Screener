ce333af961e44416279d5592c35436a11422e0d9
Dhanraj200547 <164746257+Dhanraj200547@users.noreply.github.com>
2025-07-17 22:22:55 +0530

    gradio_app.py

--- gradio_app.py
+++ gradio_app.py
@@ -47,4 +47,4 @@ interface = gr.Interface(
 )
 
 if __name__ == "__main__":
-    interface.launch(server_name="0.0.0.0", server_port=7860)
+    interface.launch(server_name="0.0.0.0", server_port=int(os.getenv("PORT", 7860)))
